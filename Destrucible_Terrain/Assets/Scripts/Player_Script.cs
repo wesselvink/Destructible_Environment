47f85b957299e888ad324e0894d44e7f998ca651
wesselvink <22092@ma-web.nl>
2018-09-26 19:29:46 +0200

    game

---
+++ Destrucible_Terrain/Assets/Scripts/Player_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D rb2D;
+	private Vector2 moveVelocity;
+	public GameObject projectile;
+
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Instantiate (projectile, transform.position, Quaternion.identity);
+		}	
+		Vector2 moveInput = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		moveVelocity = moveInput.normalized * speed;
+	}
+	void FixedUpdate(){
+		rb2D.MovePosition(rb2D.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Destrucible_Terrain/Assets/Scripts/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	private Vector2 target;
+	public float speed;
+	public int damage;
+
+	public float aereaOfEffect;
+	public LayerMask whatIsDestructible;
+
+
+
+	void Start () {
+		target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, target, speed * Time.deltaTime);
+		if (Vector2.Distance (transform.position, target) < 0.1f) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.CompareTag("Environment")){
+			Collider2D[] objectsToDamage = Physics2D.OverlapCircleAll(transform.position, aereaOfEffect, whatIsDestructible);
+			for(int i = 0; i < objectsToDamage.Length; i++){
+				objectsToDamage [i].GetComponent<Environment> ().health -= damage;
+			}
+		}
+	}
+		
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (transform.position, aereaOfEffect);
+	}
+
+}
